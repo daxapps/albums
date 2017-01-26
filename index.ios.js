655ca1b882a8174e5598f8fcbbce81649ef0352d
daxapps <daxapps777@gmail.com>
2017-01-26 11:50:43 -0600

    Initial Setup

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+// Index.ios.js - place code in here for iOS!!!!
+
+// import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a Component
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
